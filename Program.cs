b3d527074e032b2a4b3dd797a24025d83857c9c9
Marlon Salles <72519815+MarlonSallesSevero@users.noreply.github.com>
2021-12-15 23:35:21 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ExercicioQuartosHotel
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Quarto[] quartos = new Quarto[10];
+            Console.Write("Quantos Estudantes Vao se Ospedar?");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Informe quartos de 0 a 9 !!!!");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Informe o numero do quarto:");
+                int num = int.Parse(Console.ReadLine());
+                if (num <= 9 && num >= 0)
+                {
+                    Console.Write($"Informe o nome do hospede {i + 1} :");
+                    string nome = Console.ReadLine();
+                    Console.Write($"Informe o email do hospede {i + 1} :");
+                    string email = Console.ReadLine();
+                    Console.Write($"Informe a idade do hospede {i + 1}:");
+                    int idade = int.Parse(Console.ReadLine());
+                    Console.Write("Informe a diaria:");
+                    double diaria = double.Parse(Console.ReadLine());
+                    Console.WriteLine("----------------------------------------------------");
+                    quartos[num] = new Quarto(num, nome, idade, email, diaria);
+                }
+                else
+                {
+                    Console.WriteLine("Num Invalido!!!");
+                    break;
+                }
+            }
+            for (int z = 0; z < n; z++)
+            {
+                if (quartos[z] != null)
+                {
+                    Console.WriteLine($"Quarto {quartos[z].Num}, hospedado por {quartos[z].Nome}, {quartos[z].Idade} anos\n" +
+                    	$"Valor diario:{quartos[z].Diaria}\nEmail : {quartos[z].Email}\n" +
+                    		$"----------------------------------------------------------------------");
+                }
+            }
+        }
+    }
+}
---
+++ Quarto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ExercicioQuartosHotel
+{
+    public class Quarto
+    {
+        public int Num { get; private set; }
+        public string Nome { get; private set; }
+        public int Idade { get; private set; }
+        public string Email { get; private set; }
+        public double Diaria { get; private set; }
+        public Quarto(int num, string nome, int idade, string email, double diaria)
+        {
+            Num = num;
+            Nome = nome;
+            Idade = idade;
+            Email = email;
+            Diaria = diaria;
+        }
+    }
+}
